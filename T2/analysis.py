da1f4fbee0b466baa9bee9639d97668f0730abb2
Marcos Medeiros <marcos.mb.mb@outlook.com>
2024-09-20 15:28:38 -0300

    transform

--- T2/analysis.py
+++ T2/analysis.py
@@ -4,14 +4,17 @@ import matplotlib.pyplot as plt
 from scipy.stats import kurtosis, skew
 import statsmodels.api as sm
 
-def qqplot(data, title):
-    sm.qqplot(data, line='s')
-    plt.title(f'Q-Q Plot de {title}')
+def plot(data, column):
+    skewness = skew(df[column])
+    kurt = kurtosis(df[column], fisher=True)
+    fig, axs = plt.subplots(1, 2, figsize=(14, 6))
+    sns.histplot(data[column], kde=True, ax=axs[0])
+    axs[0].set_title(f'{column} - Histograma\nAssimetria: {skewness:.2f}, Curtose: {kurt:.2f}')
+    sm.qqplot(data[column], line='s', ax=axs[1])
+    axs[1].set_title(f'{column} - Q-Q Plot')
+    plt.tight_layout()
     plt.show()
 
 column = 'avaliacao_da_critica'
 df = pd.read_csv("T2/Transform.csv")
-skewness = skew(df[column])
-kurt = kurtosis(df[column], fisher=True)
-title = f'{column} Curtose: {kurt:.4f}, Assimetria: {skewness:.4f}'
-qqplot(df[column], title)
+plot(df, column)
--- T2/transform.py
+++ T2/transform.py
@@ -1,23 +1,16 @@
 import pandas as pd
 from scipy.stats import boxcox
-from scipy.stats import yeojohnson
-import numpy
 
 def transform(data):
     data['popularidade'], lamb1 = boxcox(data['popularidade'])
-    removeOutliers(data,'orcamento')
+    data['orcamento'], lamb2 = boxcox(data['orcamento'])
+    data['receita'], lamb3 = boxcox(data['receita'])
+    data['duracao'], lamb4 = boxcox(data['duracao'])
     data['avaliacao_da_critica'] = data['avaliacao_da_critica'].str.rstrip('%').astype('float') / 100
     print(lamb1)
-    data = data.dropna()
-
-def removeOutliers(data, column):
-    Q1 = data[column].quantile(0.25)
-    Q3 = data[column].quantile(0.75)
-    IQR = Q3 - Q1
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
-    mask = (data[column] >= lower_bound) & (data[column] <= upper_bound)
-    data[column] = data[column].where(mask)
+    print(lamb2)
+    print(lamb3)
+    print(lamb4)
 
 def createCSV(data, filename):
     data.to_csv(filename, index=False)
